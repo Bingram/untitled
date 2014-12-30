56c53535da2570c68d50801fe620e5bb81575bca
Brian Ingram <ingram.brian@gmail.com>
2014-12-29 23:52:52 -0800

    A little work on display area.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,9 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Brian on 12/22/2014.
  */
 public class GameBoard extends JPanel {
 
+    protected Environment theModel;
+
+
+
+    public GameBoard(){
+
+    }
+
+    public GameBoard(String title){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        try {
+
+
+        } catch (Exception e){
+            System.out.println("FAILUrE");
+        }
     }
 }
